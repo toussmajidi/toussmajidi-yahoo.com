628662bb9a55716da6cc168c0d718e6c4aa7b631
toussmajidi <toussmajidi@yahoo.com>
2020-04-01 22:17:11 -0700

    jquery

--- algorithms.js
+++ algorithms.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   // Start animation
     let $img = $("#img");
-   $("#img").keydown(function (c) {
+   $("#img").keydown( {
         if (c.keyCode == 37) {
     $img.animate({ left: "+=150px" }, 2000);
         }
   });
 
-  $("#img").keydown(function (c) {
+  $("#img").keydown( {
         if (c.keyCode == 39) {
     $img.animate({ left: "-=150px" }, 2000);
         }
-  $("#img").keydown(function (c) {
+  $("#img").keydown( {
         if (c.keyCode == 38) {
     $img.animate({ top: "+=150px" }, 2000);
         }
