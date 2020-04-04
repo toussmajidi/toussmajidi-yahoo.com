7f65a303430bf2c69af0ce6e886272e84f27b90c
toussmajidi <toussmajidi@yahoo.com>
2020-04-03 18:49:17 -0700

    Finished

--- algorithms.js
+++ algorithms.js
@@ -1,19 +1,37 @@
-$(document).ready(function() {
-  // Start animation
-    let $img = $("#img");
-   $("#img").keydown( {
-        if (c.keyCode == 37) {
-    $img.animate({ left: "+=150px" }, 2000);
-        }
+$(document).ready(() =>{
+   let $img = $(window);
+  $("#img2").hide();
+  $img.mousedown( function() {
+        $("#img1").hide();
+        $("#img2").show();
+    });
+  $img.mouseup( function() {
+        $("#img1").show();
+        $("#img2").hide();
+    });
   });
-
-  $("#img").keydown( {
-        if (c.keyCode == 39) {
-    $img.animate({ left: "-=150px" }, 2000);
-        }
-  $("#img").keydown( {
-        if (c.keyCode == 38) {
-    $img.animate({ top: "+=150px" }, 2000);
-        }
-  });
+$(document).keydown(function(e){
+  let $img = $("img");
+    switch (e.which){
+    case 37:    //left arrow key
+        $img.stop().animate({
+            left: "-=50"
+        });
+        break;
+    case 38:    //up arrow key
+        $img.stop().animate({
+            top: "-=50"
+        });
+        break;
+    case 39:    //right arrow key
+        $img.stop().animate({
+            left: "+=50"
+        });
+        break;
+    case 40:    //bottom arrow key
+        $img.stop().animate({
+            top: "+=50"
+        });
+        break;
+    }
 });
